b079625f6eb95c935d9c81750c70154e84775e9b
XuhengZhu <1906880505@qq.com>
2022-11-14 22:53:48 +0800

    Initial commit

---
+++ utils.py
@@ -0,0 +1,133 @@
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_file_list(file_position):
+    file_list = os.listdir(file_position)
+    # 删除非.dat文件 word这些
+    # print(file_list)
+    for i in range(len(file_list)-1,-1,-1):
+        if not file_list[i].endswith('.dat'):  # 检查文件类型，word什么的在处理list里删了
+            del file_list[i]  # 直接正序索引然后list.remove(element)的话这个迭代器里索引按照原来的数组索引递增，每一个for
+            # 索引加一，但是remove后本来下一个文件的实际索引会少一，所以只会处理一半的元素、
+                              #  解决办法：倒序索引
+
+    # print(file_list)
+    # 输出路径
+    out_path = file_position + '\\out\\'
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+
+    return file_list, out_path
+
+
+def get_test_condition(file_name):
+    # 获取测试条件
+    match = r'\d+\.\d+(?=mA)'  # 提取测试电流数值
+    current = float(re.search(match, file_name).group())
+    # print(current)
+    # print(type(current))
+    # print("current"+ current)
+    if not (file_name.find('1w') == -1):  # find（）返回的是找到的元素的索引，找不到返回-1，-1是True
+        fit_order = 2
+        harm_order = '1'
+    else:
+        fit_order, harm_order = 1, '2'
+
+    if not file_name.find('down')== -1:
+        Mz = 'down'
+    elif not file_name.find('up') == -1:
+        Mz = 'up'
+    else:
+        Mz = '0'  # 数据保存的时候没有表明，考虑直接删了这个数据或者画图跳过
+
+    test_condition = {'current': current, 'harm_order': harm_order, 'Mz': Mz, 'fit_value': 0}
+    return test_condition
+    # 获取条件结束
+
+
+def load_data(file_position, file_name):
+    data_name = file_position + "\\" + file_name  # 为了更改起来更方便
+
+    data_1_harm = np.loadtxt(data_name, skiprows=1, dtype=float, comments='#',
+                             unpack=False)  # unpack是一行是一行，一列是一列，false会把列变行
+
+    data = data_1_harm[:, 1:]  # #取出第二三列
+    # 去掉大场部分，先排序
+    data = data[data[:, 0].argsort()]  # 按照第一列（磁场）降序排序
+
+    return data
+
+
+def find_first_last(data, Bmax):
+    "找到小场范围磁场的数据范围"
+    # first = next(x for x in data[:, 0] if x < Bmax)
+    # last = next(x for x in data[:, 0] if x < (0-Bmax))
+    first = next(index for index, value in enumerate(data[:, 0]) if value > (-Bmax))
+    last = next(index for index, value in enumerate(reversed(data[:, 0])) if value < Bmax)
+    return [first, -1 - last]
+
+
+def fit_and_plot(file_name, data, pos, fit_result, out_path, test_condition):
+    v_w = data[:, 1]  # 谐波电压值，暂未说明几次谐波
+    B = data[:, 0]
+
+    fit_order = 0  # 拟合阶数
+    har_order = '1'  # 几次谐波，后面判断再改
+
+    # 判断是1w还是2w
+    if not (file_name.find('1w') == -1):  # 1w
+        fit_order = 2
+        har_order = '1'
+        #B = B[2*pos[0]:2*pos[1]]
+        #v_w = v_w[2*pos[0]:2*pos[1]]  # 只要小场部分
+    else:
+        fit_order, har_order = 1, '2'
+        B = B[pos[0]:pos[1]]
+        v_w = v_w[pos[0]:pos[1]]  # 只要小场部分
+    # 使用二次拟合谐波电压和场的曲线 ， 得到的f1是拟合的多项式系数，是一个数组
+    f1 = np.polyfit(B, v_w, fit_order)  # f1是一个数组，其中第一个是最高阶项拟合系数，次之次高阶 print("数据类型",type(f1))
+    # 转换成多项式
+    test_condition['fit_value'] = f1[0]
+
+    fit_result.append(test_condition)  # 写入测试条件
+
+    p1 = np.poly1d(f1, False, variable='B')
+
+    v_w_fit = p1(B)  # 拟合谐波电压值
+
+    # 绘图
+    plot1 = plt.plot(B, v_w, 's', label="original values")
+    plot2 = plt.plot(B, v_w_fit, "r", label="polyfit values")
+
+    plt.xlabel('B')
+    plt.ylabel("V_" + har_order + "w")
+    plt.legend(loc=4)  # 指定Legend的位置在右下角
+    plt.title(file_name)
+
+    figure = plt.gcf()  # 获取当前图像
+    # figure.show()
+
+    # figure.savefig("谐波.png", dpi="figure",)  # save the current figure
+    # 删除filename中的后缀名.dat
+    file_name = file_name[:-4]
+
+    figure.savefig(out_path + file_name + '.png')
+
+    figure.clear()  # 释放内存,没有的话 会把不同曲线画在一个图里，越到后面越多曲线
+
+def caculate_DL(fit_result):
+    delta_Bx=[] #计算结果
+    for i in range(len(fit_result)-1):
+        if fit_result[i]['current'] == fit_result[i+1]['current'] and fit_result[i]['Mz'] == fit_result[i+1]['Mz']:
+            if fit_result[i]['harm_order']=='1' and fit_result[i+1]['harm_order']=='2':
+                delta_Bx_1 = 0- 2 * fit_result[i+1]['fit_value']/fit_result[i]['fit_value']
+                delta_Bx.append({'current':fit_result[i]['current'],
+                                 'Mz':fit_result[i]['Mz'],
+                                 'deltaB_x':delta_Bx_1})
+    return delta_Bx
+
+            
+            
