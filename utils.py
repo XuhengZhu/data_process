20830ed57214106c108a0067189cc19fedd9f6ad
XuhengZhu <1906880505@qq.com>
2022-11-21 10:16:30 +0800

    修改了计算场的时候错误的乘了两倍

--- utils.py
+++ utils.py
@@ -8,11 +8,11 @@ def get_file_list(file_position):
     file_list = os.listdir(file_position)
     # 删除非.dat文件 word这些
     # print(file_list)
-    for i in range(len(file_list)-1,-1,-1):
+    for i in range(len(file_list) - 1, -1, -1):
         if not file_list[i].endswith('.dat'):  # 检查文件类型，word什么的在处理list里删了
             del file_list[i]  # 直接正序索引然后list.remove(element)的话这个迭代器里索引按照原来的数组索引递增，每一个for
             # 索引加一，但是remove后本来下一个文件的实际索引会少一，所以只会处理一半的元素、
-                              #  解决办法：倒序索引
+            #  解决办法：倒序索引
 
     # print(file_list)
     # 输出路径
@@ -36,7 +36,7 @@ def get_test_condition(file_name):
     else:
         fit_order, harm_order = 1, '2'
 
-    if not file_name.find('down')== -1:
+    if not file_name.find('down') == -1:
         Mz = 'down'
     elif not file_name.find('up') == -1:
         Mz = 'up'
@@ -46,6 +46,7 @@ def get_test_condition(file_name):
     test_condition = {'current': current, 'harm_order': harm_order, 'Mz': Mz, 'fit_value': 0}
     return test_condition
     # 获取条件结束
+
 
 #  同时测一次和二次谐波电压的数据
 def get_test_condition_1(file_name):
@@ -62,6 +63,7 @@ def get_test_condition_1(file_name):
     test_condition = {'current': current, 'Mz': Mz, 'fit_value': 0}
     return test_condition
 
+
 def load_data(file_position, file_name):
     data_name = file_position + "\\" + file_name  # 为了更改起来更方便
 
@@ -73,6 +75,8 @@ def load_data(file_position, file_name):
     data = data[data[:, 0].argsort()]  # 按照第一列（磁场）降序排序
 
     return data
+
+
 #   这个两种通用
 
 def find_first_last(data, Bmax):
@@ -82,6 +86,7 @@ def find_first_last(data, Bmax):
     first = next(index for index, value in enumerate(data[:, 0]) if value > (-Bmax))
     last = next(index for index, value in enumerate(reversed(data[:, 0])) if value < Bmax)
     return [first, -1 - last]
+
 
 #  单独测一次和二次
 def fit_and_plot(file_name, data, B_max, fit_result, out_path, test_condition):
@@ -95,7 +100,7 @@ def fit_and_plot(file_name, data, B_max, fit_result, out_path, test_condition):
     if not (file_name.find('1w') == -1):  # 1w
         fit_order = 2
         har_order = '1'
-        pos = find_first_last(data, 2*B_max)
+        pos = find_first_last(data, 2 * B_max)
         B = B[pos[0]:pos[1]]
         v_w = v_w[pos[0]:pos[1]]  # 只要小场部分
     else:
@@ -134,6 +139,7 @@ def fit_and_plot(file_name, data, B_max, fit_result, out_path, test_condition):
 
     figure.clear()  # 释放内存,没有的话 会把不同曲线画在一个图里，越到后面越多曲线
 
+
 #  同时测一次和二次谐波电压的数据
 def fit_and_plot_1(file_name, data, B_max, fit_result, out_path, test_condition):
     v_1w = data[:, 1]  # 原.dat第三列是二次，第四列是一次
@@ -152,7 +158,8 @@ def fit_and_plot_1(file_name, data, B_max, fit_result, out_path, test_condition)
     f1 = np.polyfit(B, v_1w, 2)  # f1是一个数组，其中第一个是最高阶项拟合系数，次之次高阶 print("数据类型",type(f1))
     f2 = np.polyfit(B, v_2w, 1)
 
-    eff_field = -0.002 * f2[0]/f1[0] # v_2w的单位是uV，1w的单位是mV，二次的相当于数值增大了1000倍因此除以1000
+    buff = 0.001*f2[0] / (f1[0] *2)
+    eff_field = -2 * buff  # v_2w的单位是uV，1w的单位是mV，二次的相当于数值增大了1000倍因此除以1000
     test_condition['fit_value'] = eff_field
 
     # 下面四行是绘图用的数据
@@ -193,34 +200,51 @@ def fit_and_plot_1(file_name, data, B_max, fit_result, out_path, test_condition)
 
     figure.clear()  # 释放内存,没有的话 会把不同曲线画在一个图里，越到后面越多曲线
 
+
 #  画有效场对于电流的图,同时测一次和二次用
-def plot_field(fit_result,out_path):
-    current, eff_field = [0,], [0,]
+def plot_field(fit_result, out_path):
+    current, eff_field = [0, ], [0, ]
     for i in range(len(fit_result) - 1):
         current.append(fit_result[i]['current'])
         eff_field.append(fit_result[i]['fit_value'])
     plot1 = plt.plot(current, eff_field, 's', label="eff_field_DL")
 
     plt.xlabel('I')
-    plt.ylabel("B_DL" )
+    plt.ylabel("B_DL")
     plt.legend(loc=4)  # 指定Legend的位置在右下角
     plt.title('B_DL vs I')
     figure = plt.gcf()  # 获取当前图像
-    file_name = 'Mz ='+fit_result[0]['Mz']+'result'
+    file_name = 'Mz =' + fit_result[0]['Mz'] + 'result'
     figure.savefig(out_path + file_name + '.png')
     figure.clear()  # 释放内存,没有的话 会把不同曲线画在一个图里，越到后面越多曲线
 
 
 def caculate_DL(fit_result):
-    delta_Bx=[] #计算结果
-    for i in range(len(fit_result)-1):
-        if fit_result[i]['current'] == fit_result[i+1]['current'] and fit_result[i]['Mz'] == fit_result[i+1]['Mz']:
-            if fit_result[i]['harm_order']=='1' and fit_result[i+1]['harm_order']=='2':
-                delta_Bx_1 = 0- 2 * fit_result[i+1]['fit_value']/fit_result[i]['fit_value']
-                delta_Bx.append({'current':fit_result[i]['current'],
-                                 'Mz':fit_result[i]['Mz'],
-                                 'deltaB_x':delta_Bx_1})
+    delta_Bx = []  # 计算结果
+    for i in range(len(fit_result) - 1):
+        if fit_result[i]['current'] == fit_result[i + 1]['current'] and fit_result[i]['Mz'] == fit_result[i + 1]['Mz']:
+            if fit_result[i]['harm_order'] == '1' and fit_result[i + 1]['harm_order'] == '2':
+                delta_Bx_1 = 0 - 2 * fit_result[i + 1]['fit_value'] / (2*fit_result[i]['fit_value'])
+                delta_Bx.append({'current': fit_result[i]['current'],
+                                 'Mz': fit_result[i]['Mz'],
+                                 'deltaB_x': delta_Bx_1})
     return delta_Bx
 
-            
-            
+
+# 保存分别测量的数据
+def B_DL_result_save(delta_Bx, path):
+    full_path = path + '\\result' + '.txt'
+    file = open(full_path, 'w')
+    for i in range(len(delta_Bx) - 1):
+        if delta_Bx[i]['Mz'] == 'up':
+            current, B_DL = delta_Bx[i]['current'], delta_Bx[i]['deltaB_x']
+            file.write(str(current) + '    ' + str(B_DL))
+            file.write('\n')
+    file.write('\n' + '\n')
+    for i in range(len(delta_Bx) - 1):
+        if delta_Bx[i]['Mz'] == 'down':
+            current, B_DL = delta_Bx[i]['current'], delta_Bx[i]['deltaB_x']
+            file.write(str(current) + '    ' + str(B_DL))
+            file.write('\n')
+    file.close()
+    print("保存文件成功")
